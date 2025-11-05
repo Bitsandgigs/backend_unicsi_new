0b5e0e8cf8cb85ac95d9dda6bba99113da31a3c7
moaaz-cse <moaazcse@gmail.com>
2025-11-05 15:04:22 +0530

    reverting back to deploy on the sever instance

--- src/server.js
+++ src/server.js
@@ -55,4 +55,4 @@ app.get("/", (req, res) => {
 
 // Start Express server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
